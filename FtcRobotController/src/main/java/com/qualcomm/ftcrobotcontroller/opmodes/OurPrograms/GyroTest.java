b64f05b7977c098d768eae80844e6961aa32fd93
walt0194 <waltb18082@isd273.org>
2016-02-24 09:29:39 -0600

    Gyro Testing program with p control

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurPrograms/GyroTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurPrograms/GyroTest.java
@@ -6,37 +6,54 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.hardware.ModernRoboticsI2cGyro;
 
 public class GyroTest extends LinearOpMode {
 
-    DcMotor rwa; // P0 port 1
-    DcMotor rwb; // P0 port 2
-    DcMotor liftL; // P1 port 1
-    DcMotor liftR; // P1 port 2
-    Servo leftComb; // P2 channel 1
-    Servo rightComb; // P2 channel 2
-    Servo trigL; // P2 channel 3
-    Servo trigR; // P2 channel 4
-    DcMotor scoopArm; // P3 port 1
-    //DcMotor winch; - Winch not currently on robot will be //P3 port 2
-    //Servo leftCR; // Not on robot- P4 channel 1
-    //Servo rightCR; //Not on robot- P4 channel 2
-    Servo wrist;
-    Servo ddspivot; // P4 channel 3
-    Servo ddsclaw; // P4 channel 4
-    DcMotor lwa; // P5 port 1
-    DcMotor lwb; // P5 port 2
+    DcMotor rwa; //rightwheels
+    DcMotor rwb;
 
-    GyroSensor Gyro;
+    DcMotor lwa; //leftwheels
+    DcMotor lwb;
 
-    int heading = Gyro.getHeading();
+    DcMotor liftL;
+    DcMotor liftR;
+
+    Servo dump; //servo to score with dumper
+    Servo swivel; //servo that 'spreads' with dumper
+
+    Servo holdR; // churro holders
+    Servo holdL;
+
+    Servo trigL; //zip-line triggers
+    Servo trigR;
+
+    Servo holdC; //servo to guard lift
+
+    DcMotor collector; //bottom collector motor
+    DcMotor collector2; //top collector motor
+
+    Servo dds;
+
+    ModernRoboticsI2cGyro Gyro;
+
+    double heading = Gyro.getHeading();
+    double integratedHeading = Gyro.getIntegratedZValue();
+    double headingError;
+    double targetHeading;
+    double drivesteering;
+    double driveGain = 0.17;
+    double leftPower;
+    double rightPower;
+    double midPower = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        lwa = hardwareMap.dcMotor.get("leftwheelA");
+        lwa = hardwareMap.dcMotor.get("leftwheelA"); //leftwheels
         lwb = hardwareMap.dcMotor.get("leftwheelB");
-        rwa = hardwareMap.dcMotor.get("rightwheelA");
+
+        rwa = hardwareMap.dcMotor.get("rightwheelA"); //rightwheels
         rwb = hardwareMap.dcMotor.get("rightwheelB");
         rwa.setDirection(DcMotor.Direction.REVERSE);
         rwb.setDirection(DcMotor.Direction.REVERSE);
@@ -45,29 +62,68 @@ public class GyroTest extends LinearOpMode {
         liftR = hardwareMap.dcMotor.get("liftR");
         liftR.setDirection(DcMotor.Direction.REVERSE);
 
-        scoopArm = hardwareMap.dcMotor.get("scoopArm");
+        collector = hardwareMap.dcMotor.get("collector");
+        collector2 = hardwareMap.dcMotor.get("collector2");
 
-        wrist = hardwareMap.servo.get("wrist");
+        dump = hardwareMap.servo.get("dump"); //dumper control
+        swivel = hardwareMap.servo.get("swivel");
 
-        //rightCR = hardwareMap.servo.get("rightCR");
-
-        leftComb = hardwareMap.servo.get("leftComb");
-        rightComb = hardwareMap.servo.get("rightComb");
-
-        trigL = hardwareMap.servo.get("trigL");
+        trigL = hardwareMap.servo.get("trigL"); //triggers
         trigR = hardwareMap.servo.get("trigR");
 
-        ddspivot = hardwareMap.servo.get("ddspivot");
-        ddsclaw = hardwareMap.servo.get("ddsclaw");
+        dds = hardwareMap.servo.get("dds");
+
+        holdL = hardwareMap.servo.get("holdL"); //churro holder
+        holdR = hardwareMap.servo.get("holdR");
+
+        holdC = hardwareMap.servo.get("holdC"); //lift holder
+
+        //***INIT***//
+
+        dump.setPosition(0);
+        swivel.setPosition(1);
+
+        trigL.setPosition(0.8);
+        trigR.setPosition(0.05);
+
+        dds.setPosition(1);
+
+        holdL.setPosition(0.75);
+        holdR.setPosition(0.05);
+
+        holdC.setPosition(1);
+
+        Gyro.calibrate();
 
         waitForStart();
 
+        while (Gyro.isCalibrating()) {
+            Thread.sleep(50);
+            telemetry.addData("Gyro is Calibrating", "Gyro is Calibrating");
+        }
+
+
         while(heading < 45)
         {
-            lwa.setPower(0.8);
-            lwb.setPower(0.8);
-            rwb.setPower(0.8);
-            rwa.setPower(0.8);
+            heading = Gyro.getHeading();
+            integratedHeading = Gyro.getIntegratedZValue();
+
+            telemetry.addData("heading", heading);
+            telemetry.addData("Integrated", integratedHeading);
+
+            targetHeading = 45;
+            headingError = targetHeading - heading;
+
+            drivesteering = driveGain * headingError;
+
+            leftPower = midPower + drivesteering;
+            rightPower = midPower - drivesteering;
+
+            lwa.setPower(leftPower);
+            lwb.setPower(leftPower);
+            rwa.setPower(rightPower);
+            rwb.setPower(rightPower);
+
         }
 
     }
